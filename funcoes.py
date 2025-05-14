9e1ac6232b89a8e72166e8a4bf5fcbbdf8cad8a8
guilhermetlmelo <guilhermetlm096@gmail.com>
2025-05-14 19:19:42 -0300

    extra

--- funcoes.py
+++ funcoes.py
@@ -1,4 +1,4 @@
-import random 
+import random
 
 def adicionar_wod():
     d = input("Data (DD/MM/AAAA): ")
@@ -7,8 +7,7 @@ def adicionar_wod():
     mov = input("Movimentos: ")
     with open("treino.txt", "a", encoding="utf-8") as f:  # adiciona no wods.csv os inputs acima
         f.write(f"{d};{t};{dur};{mov}\n")
-    print("treino adicionado com sucesso!")
-
+    print("treino adicionado com sucesso!")
 
 def ver_wod():
     try:
@@ -18,7 +17,7 @@ def ver_wod():
             print("nao possui treinos")
             return
         for i, linha in enumerate(linhas):
-            partes = linha.strip().split(";")  # divide
+            partes = linha.strip().split(", ")  # divide
             d = partes[0]
             t = partes[1]
             dur = partes[2]
@@ -38,7 +37,7 @@ def editar_wod():
             return
 
         for i, linha in enumerate(linhas):
-            dados = linha.strip().split(";")
+            dados = linha.strip().split(",")
             d, t, dur = dados[0], dados[1], dados[2]
             mov = ", ".join(dados[3:])
             print(
@@ -66,31 +65,28 @@ def editar_wod():
         print("arquivo não encontrado")
     except ValueError:
         print("digite um numero")
-
-
-
 def excluir_wod():
     try:
         with open("treino.txt", "r", encoding="utf-8") as f:
             linhas = f.readlines()
 
         if len(linhas) == 0:
-            print("Não existem wods")
+            print("não existem wods")
             return
 
        
         for i, linha in enumerate(linhas):
-            partes = linha.strip().split(";")
+            partes = linha.strip().split(",")
             d = partes[0]
             t = partes[1]
             dur = partes[2]
             mov = ", ".join(partes[3:])
-            print(f"{i}. data: {d} ; tipo: {t} ; duração: {dur}min ; movimentos: {mov}")
+            print(f"{i+1}. data: {d} ; tipo: {t} ; duração: {dur}min ; movimentos: {mov}")
 
         
-        n_exc = int(input("Digite o número do WOD que deseja excluir: "))
+        n_exc = int(input("Digite o número do WOD que deseja excluir: ")) - 1
 
-        if n_exc < 0 or n_exc > len(linhas):
+        if n_exc < 0 or n_exc >= len(linhas):
             print("digite outro numero")
             return
 
@@ -104,8 +100,6 @@ def excluir_wod():
         print("arquivo não encontrado")
     except ValueError:
         print("entrada inválida")
-
-
 def adicionar_meta():
     obj = input("digite a meta desejada: ")
     prazo = input("digite o prazo (DD/MM/AAAA) para concluir a meta: ")
@@ -115,9 +109,7 @@ def adicionar_meta():
         f.write(f"{obj},{prazo},{status}\n")
 
     print("sua meta foi adicionada")
-
-
-def ver_meta(): 
+def ver_meta():
     try:
         with open("meta.txt", "r", encoding="utf-8") as f:  # faz a leitura do arquivo
             linhas = f.readlines()  # conta quantas linhas possuem no arquivo
@@ -125,7 +117,7 @@ def ver_meta():
             print("nao possui metas")
             return
         for i, linha in enumerate(linhas):
-            partes = linha.strip().split(";")  # divide
+            partes = linha.strip().split(", ")  # divide
             obj = partes[0]
             prazo = partes[1]
             status = partes[2]
@@ -134,6 +126,20 @@ def ver_meta():
             print(f"{i+1}. {obj}; {prazo}; {status};\n")
     except FileNotFoundError:
         print("arquivo não encontrado")
+
+
+def frase_motivacional():
+    try:
+        with open("frases.txt", "r", encoding="utf-8") as f:
+            frases = f.readlines()
+    
+        frase = random.choice(frases).strip()
+        print("\n Frase motivacional do dia:")
+        print(f"  {frase}\n")
+
+    except FileNotFoundError:
+        print("arquivo frases.txt não encontrado")
+
         
 
 def filtro(file):  # recebe o caminho do arquivo e retorna uma lista de treinos filtrados
@@ -203,6 +209,8 @@ def filtro(file):  # recebe o caminho do arquivo e retorna uma lista de treinos 
 
 
 
+
+
 def selecionar(file): #Retorna um treino selecionado em formado de uma lista
     try:
         with open(file, "r", encoding="utf8") as arquivo:
@@ -237,30 +245,5 @@ def selecionar(file): #Retorna um treino selecionado em formado de uma lista
 
 
 
-def sugestao_aleatoria3(): # Retorna uma lista de 3 exercicios aleatorios de acordo com o tipo escolhido pelo usuario.
-    try:
-        def sorteio(exercicios_salvos):
-            if len(exercicios_salvos) > 3:
-                selecionados = []
-                for _ in range(3):  # Seleciona 3 aleatórios sem repetição
-                    indice = random.randint(0, len(exercicios_salvos) - 1)
-                    selecionados.append(exercicios_salvos.pop(indice))
-                return selecionados
-            else:
-                return exercicios_salvos
 
 
-        # 0 - ombro/ 1- Peito/ 2- costas/ 3- braço/ 4-pernas
-        sugestoes = [["desenvolvimento","elevação lateral", "elevação frontal","Crucifixo inverso","Remada Alta","Pulley Articulado"],["supino reto","supino inclinado","Crucifixo reto","Crossover","Banch press","Crucifixo inclinado"],["remada alta","puxada","pulldown","Remada baixa", "Remada curvada","Levantamento terra"],["Rosca","Triceps Françês","Triceps na corda","Rosca Scott","Triceps testa","Mergulho"],["leg press", "agachamento livre", "agachamento com barra","flexora","Abdutora","adutora"]]
-        
-        
-        print("Digite o treino que voce quer fazer:\n1- Ombro\n2- Peito\n3- costas\n4- Braço\n5- Pernas")
-        opcao = int(input("Digite o numero que corresponde a opcao: ")) -1
-        
-        if 0 <= opcao < len(sugestoes): 
-            print(sorteio(sugestoes[opcao]))
-        else: 
-            print("Voce nao inseriu uma opção válida")
-    except ValueError:
-        print("Digite um número!")
-        
