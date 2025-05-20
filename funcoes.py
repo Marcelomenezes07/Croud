cbee5a0394f9466bc39f140a1a8dcc42e897fc58
mmarc3lo <marceloamenezes07@gmail.com>
2025-05-19 22:48:08 -0300

    Correção de Bugs

--- funcoes.py
+++ funcoes.py
@@ -7,7 +7,7 @@ def adicionar_wod():
     dur = input("Duração em min: ")
     mov = input("Movimentos: ")
     if d and t and dur and mov:
-        with open("treino.txt", "a", encoding="utf-8") as f:  # adiciona no wods.csv os inputs acima
+        with open("treino.txt", "a", encoding="utf-8") as f:  
             f.write(f"{d};{t};{dur};{mov}\n")
         print("treino adicionado com sucesso!")
     else:
@@ -22,7 +22,7 @@ def ver_wod():
             return
         for i, treino in enumerate(treinos):
                 dados = treino.strip().split(";") # separa os movimentos do treino
-                if len(treino) >= 4:
+                if len(dados) >= 4:
                     print(f"{i+1:^4} ---> Data: {dados[0]}\t Tipo: {dados[1]}\t Duração: {dados[2]}\t Movimentos: {dados[3]}")
 
     except FileNotFoundError:
@@ -38,8 +38,8 @@ def editar_wod():
             return
 
         
-        n_tro = linhas.index(selecionar())
-
+        n_tro = linhas.index(selecionar()) #n_tro é o numero de troca
+            # A funcao selecionar retorna o treino que o usuario irá selecionar de forma interativa, após isso é visto o index dela no conjunto total de treino 
         if n_tro < 0 or n_tro >= len(linhas):
             print("digite outro numero")
             return
@@ -49,7 +49,7 @@ def editar_wod():
         ndur = input("digite a nova duração: ")
         nmov = input("digite os novos movimentos (separados por vírgula): ")
 
-        nova_linha = f"{nd};{nt};{ndur};{nmov}\n"
+        nova_linha = f"{nd};{nt};{ndur};{nmov}\n" #
         linhas[n_tro] = nova_linha
 
         with open("treino.txt", "w", encoding="utf-8") as f:
@@ -125,70 +125,71 @@ def filtro():  # recebe o caminho do arquivo e retorna uma lista de treinos filt
     except FileNotFoundError:
         print("Arquivo 'treino.txt' não encontrado.")
         return []
+    try:
+        filtrado = treinos  # lista para armazenar os treinos filtrados
+        filtros = input("Digite qual o filtro você deseja usar para buscar o treino:\n1. Data\n2. Tipo\n3. Duração\n4. Movimento\n").strip().replace(" ","").split(",")
 
-    filtrado = treinos  # lista para armazenar os treinos filtrados
-    filtros = input("Digite qual o filtro você deseja usar para buscar o treino:\n1. Data\n2. Tipo\n3. Duração\n4. Movimento\n").strip().replace(" ","").split(",")
+        for filtro in filtros:
+            filtro_temporario = []
 
-    for filtro in filtros:
-        filtro_temporario = []
+            # Filtro por Data
+            if filtro == "1":
+                data = input("Digite a data do treino (ex: 20/04): ").strip()
+                filtrado = [treino for treino in filtrado if treino[0].strip() == data]
 
-        # Filtro por Data
-        if filtro == "1":
-            data = input("Digite a data do treino (ex: 20/04): ").strip()
-            filtrado = [treino for treino in filtrado if treino[0].strip() == data]
+            # Filtro por Tipo
+            elif filtro == "2":
+                tipo = input("Digite o tipo do treino (ex: perna, ombros...): ").strip().lower()
+                filtrado = [treino for treino in filtrado if treino[1].strip().lower() == tipo]
 
-        # Filtro por Tipo
-        elif filtro == "2":
-            tipo = input("Digite o tipo do treino (ex: perna, ombros...): ").strip().lower()
-            filtrado = [treino for treino in filtrado if treino[1].strip().lower() == tipo]
+            # Filtro por Duração
+            elif filtro == "3":
+                duracao_opcao = input("Digite 1 ou 2 a seguir:\n1 - Buscar duração exata\n2 - Buscar por intervalo de duração\n").strip()
+                
+                # duração exata
+                if duracao_opcao == "1":
+                    duracao = input("Digite a duração exata (em minutos): ").strip()
+                    filtrado = [treino for treino in filtrado if treino[2].strip() == duracao]
 
-        # Filtro por Duração
-        elif filtro == "3":
-            duracao_opcao = input("Digite 1 ou 2 a seguir:\n1 - Buscar duração exata\n2 - Buscar por intervalo de duração\n").strip()
-            
-            # duração exata
-            if duracao_opcao == "1":
-                duracao = input("Digite a duração exata (em minutos): ").strip()
-                filtrado = [treino for treino in filtrado if treino[2].strip() == duracao]
+                # duração por intervalo
+                elif duracao_opcao == "2": 
+                    try:
+                        i1 = int(input("Digite o menor número do intervalo de duração (em minutos): "))
+                        i2 = int(input("Digite o maior número do intervalo de duração (em minutos): "))
+                        for treino in filtrado:
+                            try:
+                                tempo = int(treino[2].strip())  # treino[2] é a duração
+                                if i1 <= tempo <= i2:
+                                    filtro_temporario.append(treino)
+                            except ValueError:
+                                continue  # ignora treinos com duração inválida
+                        filtrado = filtro_temporario
+                    except ValueError:
+                        print("Valores inválidos para intervalo de duração.")
 
-            # duração por intervalo
-            elif duracao_opcao == "2": 
-                try:
-                    i1 = int(input("Digite o menor número do intervalo de duração (em minutos): "))
-                    i2 = int(input("Digite o maior número do intervalo de duração (em minutos): "))
-                    for treino in filtrado:
-                        try:
-                            tempo = int(treino[2].strip())  # treino[2] é a duração
-                            if i1 <= tempo <= i2:
-                                filtro_temporario.append(treino)
-                        except ValueError:
-                            continue  # ignora treinos com duração inválida
-                    filtrado = filtro_temporario
-                except ValueError:
-                    print("Valores inválidos para intervalo de duração.")
+                else:
+                    print("Opção de duração inválida!")
+
+            # Filtro por Movimento
+            elif filtro == "4":
+                movimento_buscado = input("Digite o movimento que você deseja encontrar: ").strip().lower()
+                for treino in filtrado:
+                    movimentos = treino[3].split(",")
+                    movimentos = [movimento.strip().lower() for movimento in movimentos]
+                    if movimento_buscado in movimentos:
+                        filtro_temporario.append(treino)
+                filtrado = filtro_temporario
 
             else:
-                print("Opção de duração inválida!")
-
-        # Filtro por Movimento
-        elif filtro == "4":
-            movimento_buscado = input("Digite o movimento que você deseja encontrar: ").strip().lower()
-            for treino in filtrado:
-                movimentos = treino[3].split(",")
-                movimentos = [movimento.strip().lower() for movimento in movimentos]
-                if movimento_buscado in movimentos:
-                    filtro_temporario.append(treino)
-            filtrado = filtro_temporario
-
+                print("Filtro não encontrado!")
+        
+        if filtrado == []:
+            print("Não foi encontrado nenhum treino com esses filtros")
         else:
-            print("Filtro não encontrado!")
-    
-    if filtrado == []:
-        print("Não foi encontrado nenhum treino com esses filtros")
-    else:
-        filtrado = [";".join(treino) for treino in filtrado] 
-        return filtrado
-
+            filtrado = [";".join(treino) for treino in filtrado] 
+            return filtrado
+    except ValueError: 
+        print("Digite corretamente a opção citada")
 
 def selecionar(): #Retorna um treino selecionado em formado de uma lista
     try:
--- main.py
+++ main.py
@@ -18,73 +18,65 @@ while True:
     except ValueError:
         print("O valor digitado nao corresponde a uma das opções acima")
         continue
+
     match opcao:
         case 1: 
             os.system('cls')
             print(f"{formato['negrito']}Você escolheu adicionar Wod\n{formato['padrao']}")
             funcoes.adicionar_wod()
-            input("Pressione enter para voltar para o menu\n")
 
         case 2:
             os.system('cls')
             print(f"{formato['negrito']}Você escolheu ver wod\n{formato['padrao']}")
             funcoes.ver_wod()
-            input("Pressione enter para voltar para o menu\n")
 
         case 3:
             os.system('cls')
             print(f"{formato['negrito']}Você escolheu editar Wod{formato['padrao']}")
             funcoes.editar_wod()
-            input("Pressione enter para voltar para o menu\n")
 
         case 4:
             os.system('cls')
             print(f"{formato['negrito']}Você escolheu excluir Wod{formato['padrao']}")
             funcoes.excluir_wod()
-            input("Pressione enter para voltar para o menu\n")
 
         case 5:
             os.system('cls')
             print(f"{formato['negrito']}Você escolheu filtrar Wods{formato['padrao']}")
             resultado = funcoes.filtro()
             if resultado: funcoes.exibir(resultado)
-            input("Pressione enter para voltar para o menu\n")
 
         case 6:
             os.system('cls')
             print(f"{formato['negrito']}Você escolheu adicionar metas{formato['padrao']}")
             funcoes.adicionar_meta()
-            input("Pressione enter para voltar para o menu\n")
 
         case 7:
             os.system('cls')
             print(f"{formato['negrito']}Você escolheu ver metas{formato['padrao']}")
             funcoes.ver_meta()
-            input("Pressione enter para voltar para o menu\n")
 
         case 8:
             os.system('cls')
             print(f"{formato['negrito']}Você escolheu atualizar metas{formato['padrao']}")
             funcoes.atualizar_meta()
-            input("Pressione enter para voltar para o menu\n")
 
         case 9:
             os.system('cls')
             print(f"{formato['negrito']}Você escolheu receber uma sugestao de exercicios aleatorios{formato['padrao']}")
             funcoes.sugestao_aleatoria3()
-            input("Pressione enter para voltar para o menu\n")
 
         case 10:
             os.system('cls')
             print(f"{formato['negrito']}Você escolheu frase motivacionais{formato['padrao']}")
             funcoes.frase_motivacional()
-            input("Pressione enter para voltar para o menu\n")
 
         case 0:
             print(f"{formato['negrito']}Você escolheu encerrar o programa{formato['padrao']}")
             break
+
         case _: 
             print(f"{formato['negrito']}A opção digitada não se encontra no menu de opções{formato['padrao']}")
-            input("Pressione enter para voltar para o menu\n")
-    
+
+    input("Pressione enter para voltar para o menu\n")
     os.system('cls')
